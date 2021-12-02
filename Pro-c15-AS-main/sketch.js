85f0a4e2764217b8187e28f2da386078d1b9376d
ThiagoAlves13 <93220975+ThiagoAlves13@users.noreply.github.com>
2021-12-02 15:10:55 -0300

    Add files via upload

---
+++ Pro-c15-AS-main/sketch.js
@@ -0,0 +1,25 @@
+var ball;
+var vx = 2;
+
+function setup() {
+  createCanvas(400,400);
+
+  ball = createSprite(100,100,100,100);
+
+  ball.setCollider ("rectangle",0,0,100,100 )
+  ball.debug = true;
+}
+
+function draw() 
+{
+  background(51);
+  if(ball.position.x<=50 || ball.position.x>=350)
+  {
+    vx= -vx;
+  }
+  ball.velocity.x = vx;
+
+  drawSprites();
+
+}
+
